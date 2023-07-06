a8202cfc8877a21899a5d4a07b3a0e3d96f1454f
kaiqueD17 <129909829+kaiqueD17@users.noreply.github.com>
2023-07-05 21:54:17 -0300

    newprojetin
    xalalallalla

---
+++ tabelabrasileirao/jogos/js/tabela.js
@@ -0,0 +1,24 @@
+function createGame(player1,hour,player2){
+    return `
+    <li>
+    <img src="./times/icon-${player1}.png" alt="Bandeira do ${player1}"/>
+    <strong>${hour}</strong>
+    <img src="./times/icon-${player2}.png" alt="Bandeira do ${player2}"/>
+
+    `
+}
+let delay = -0.4;
+function createCard(date,day,games){
+    delay = delay + 0.4;
+    return`
+    <div class="card" style="animation-delay:${delay}s">
+    <h2>${date}<span>${day}</span></h2>
+    <ul>
+    ${games}
+    </ul>
+    </div>
+    `
+
+}
+document.querySelector("#cards").innerHTML = 
+createCard("05/07","Quarta-Feira",createGame("corinthians","21:00","palmeiras"));
